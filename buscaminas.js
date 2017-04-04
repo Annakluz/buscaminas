c2e57927db51b21eb750abfa4c56547592e51156
Anna karina Luz <anna.luz.duran@gmail.com>
2017-04-04 12:52:55 -0500

    Arregladas dos primeras funciones

--- buscaminas.js
+++ buscaminas.js
@@ -1,14 +1,12 @@
-//variables globales
-var arregloImagenes = []; //Declaro el arreglo donde iran las imagenes
-//function que inicia las bombas en el archivo
-function mordidasZombie(){
+ 
+var arreglo = [];
+function bombas(){
     var i=0;
-    for(i=0 i<16 i++) {
-        arregloImagenes[i] = "zombie02.jpg"
+    for(i=0; i<=16; i++){ 
+      arreglo[i]= "zombi2.jpg";  
     }
+    
 }
-
-function abrirCelda(imagen) {
-    imagen.src = arregloImagenes[0];
-    alert("click!");
+function abrir(imagen) {
+    imagen.src = arreglo[0]; //cambia las imagenes. Todo bien hasta este punto.
 }
